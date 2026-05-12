0e9f62a06037c3c701250d799488944b56adce97
j-jaanah <134391969+j-jaanah@users.noreply.github.com>
2026-05-12 19:26:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
+function showJoinGame() {
+
+  document.getElementById("homeScreen").style.display = "none";
+  document.getElementById("joinScreen").style.display = "block";
+
+}
+
+function showNewGame() {
+
+  document.getElementById("homeScreen").style.display = "none";
+  document.getElementById("newGameScreen").style.display = "block";
+
+}
+
 function joinGame() {
 
-  const gameId = document.getElementById("gameId").value;
-  const playerName = document.getElementById("playerName").value;
+  const gameId =
+    document.getElementById("joinGameId").value;
+
+  const playerName =
+    document.getElementById("joinPlayerName").value;
 
   if (!gameId || !playerName) {
-    alert("Please enter Game ID and Player Name");
+
+    alert("Please fill all fields");
     return;
+
   }
 
   alert(
@@ -13,4 +32,22 @@ function joinGame() {
     "\nPlayer: " + playerName
   );
 
-}console.log("Website loaded");
+}
+
+function createGame() {
+
+  const hostName =
+    document.getElementById("hostName").value;
+
+  if (!hostName) {
+
+    alert("Please enter host name");
+    return;
+
+  }
+
+  alert(
+    "Game created by: " + hostName
+  );
+
+}
