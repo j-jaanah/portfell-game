43a7006b82775dbc9a78a9ef7d4adc099f11688d
j-jaanah <134391969+j-jaanah@users.noreply.github.com>
2026-05-12 19:21:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Website loaded");
+function joinGame() {
+
+  const gameId = document.getElementById("gameId").value;
+  const playerName = document.getElementById("playerName").value;
+
+  if (!gameId || !playerName) {
+    alert("Please enter Game ID and Player Name");
+    return;
+  }
+
+  alert(
+    "Joined game: " + gameId +
+    "\nPlayer: " + playerName
+  );
+
+}console.log("Website loaded");
